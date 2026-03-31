73b8f83b224324af2b8ae46ff95a7536aba68a65
ErikLow <erik.low@colorado.edu>
2026-03-31 13:11:28 -0600

    Update NACA.m
    changing x_angle to x, N-1 panels to N panels

--- NACA.m
+++ NACA.m
@@ -1,29 +1,29 @@
 function [x_b,y_b] = NACA(m,p,t,c,N)
 
-x_angle = linspace(0,c,N);
+x = linspace(0,c,N+1);
 
 
-y_t = (t/0.2)*c.*(0.2969.*sqrt(x_angle./c)-0.1260.*(x_angle./c)-0.3516.*(x_angle./c).^2+0.2843.*(x_angle./c).^3-0.1036.*(x_angle./c).^4);
+y_t = (t/0.2)*c.*(0.2969.*sqrt(x./c)-0.1260.*(x./c)-0.3516.*(x./c).^2+0.2843.*(x./c).^3-0.1036.*(x./c).^4);
 
 
 
-yc = zeros(1,length(x_angle));
-dy_dx = zeros(1,length(x_angle));
+yc = zeros(1,length(x));
+dy_dx = zeros(1,length(x));
 
-for i = 1:N
-    if x_angle(i) < p * c
-        yc(i) = m .* (x_angle(i)./p^2).*((2*p)-x_angle(i)./c);
-        dy_dx(i) = (m / p^2) .* (2*p - x_angle(i)./c)+(m.*x_angle(i)./(p^2)).*(-1/c);
+for i = 1:(N+1)
+    if x(i) < p * c
+        yc(i) = m .* (x(i)./p^2).*((2*p)-x(i)./c);
+        dy_dx(i) = (m / p^2) .* (2*p - x(i)./c)+(m.*x(i)./(p^2)).*(-1/c);
     else
-        yc(i) = m .* ((c - x_angle(i)) ./ (1-p)^2) .* (1 + x_angle(i)./c - 2*p);
-        dy_dx(i) = (-m / (1-p)^2)*(1+x_angle(i)./c-2*p)+((m.*(c-x_angle(i))./(1-p)^2).*(1/c));
+        yc(i) = m .* ((c - x(i)) ./ (1-p)^2) .* (1 + x(i)./c - 2*p);
+        dy_dx(i) = (-m / (1-p)^2)*(1+x(i)./c-2*p)+((m.*(c-x(i))./(1-p)^2).*(1/c));
     end
 end
 
 A = atan(dy_dx);
 
-xu = x_angle - y_t.*(sin(A));
-xl = x_angle + y_t.*(sin(A));
+xu = x - y_t.*(sin(A));
+xl = x + y_t.*(sin(A));
 
 yu = yc + y_t.*(cos(A));
 yl = yc - y_t.*(cos(A));
@@ -32,8 +32,8 @@ yl = yc - y_t.*(cos(A));
 
 figure;
 hold on
-plot(x_angle, y_t, "Color",'r');
-plot(x_angle,yc,"Color",'b');
+plot(x, y_t, "Color",'r');
+plot(x,yc,"Color",'b');
 plot(xu,yu,"Color",'k');
 plot(xl,yl,"Color",'k');
 ylim([-0.5,0.5])
